d566b1a98dbb575b6432c15791fb87278ffb07ca
ritikjoshi47 <ritikjoshi.info@gmail.com>
2026-02-08 18:59:37 +0100

    Added 2nd Stage and Fill Colors

--- src/be/kdg/progpat/m1/HelloJavaFX.java
+++ src/be/kdg/progpat/m1/HelloJavaFX.java
@@ -1,9 +1,12 @@
 package be.kdg.progpat.m1;
 
 import javafx.application.Application;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -15,14 +18,26 @@ public class HelloJavaFX extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label helloLabel = new Label("Hello JavaFX!");
-        BorderPane root = new BorderPane(helloLabel);
-        Scene scene = new Scene(root);
-        primaryStage.setWidth(500);
-        primaryStage.setHeight(600);
+//        Label helloLabel = new Label("Hello JavaFX!");
+//        BorderPane root = new BorderPane(helloLabel);
+        Group root = new Group();
+        Scene scene = new Scene(root,50,600);
+
+        scene.setFill(Color.YELLOW);
+        scene.setCursor(Cursor.CROSSHAIR);
+
+
+        Scene scene1 = new Scene(new Group(), 900, 800);
+        scene1.setCursor(Cursor.HAND);
+        scene1.setFill(Color.RED);
+        Stage secondStage = new Stage();
+        secondStage.setTitle("2nd Stage");
+        secondStage.setScene(scene1);
+        secondStage.show();
         primaryStage.setResizable(true);
-        primaryStage.setTitle("Hey! This is the title");
+
         primaryStage.setMaxWidth(700);
+        primaryStage.setTitle("Hey! This is the title");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
