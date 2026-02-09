473c96fcba2aa0511f348eeff668db53e60932cc
ritikjoshi47 <ritikjoshi.info@gmail.com>
2026-02-09 19:26:31 +0100

    Changed LabelView1 to LabelView2 to see result

--- src/be/kdg/progpat/week2/m2/testapp/Main.java
+++ src/be/kdg/progpat/week2/m2/testapp/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage){
         TestAppModel model = new TestAppModel();
-        LabelView1 view = new LabelView1();
+        LabelView2 view = new LabelView2();
         primaryStage.setScene(new Scene(view));
         primaryStage.setTitle("Background");
         new TestAppPresenter(model, view);
