855064fa966274c29d0600c58f674413f203560a
ritikjoshi47 <ritikjoshi.info@gmail.com>
2026-02-12 10:59:22 +0100

    Added pop-up window when closing the screen

--- src/be/kdg/progpat/week2/m2/testapp/view/TestAppPresenter.java
+++ src/be/kdg/progpat/week2/m2/testapp/view/TestAppPresenter.java
@@ -1,6 +1,14 @@
 package be.kdg.progpat.week2.m2.testapp.view;
 
 import be.kdg.progpat.week2.m2.testapp.model.TestAppModel;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.WindowEvent;
+
+import java.util.Optional;
+
+import static javafx.scene.control.Alert.AlertType.CONFIRMATION;
 
 public class TestAppPresenter {
     private TestAppModel model;
@@ -18,9 +26,23 @@ public class TestAppPresenter {
         // lambdas) to view controls.
         // In the event handlers: call model methods
         // and updateView().
+        view.getScene().getWindow().setOnCloseRequest(new ConfirmCloseHandler());
     }
 
     private void updateView() {
         // fills the view with model data
+    }
+    private class ConfirmCloseHandler implements EventHandler<WindowEvent> {
+        @Override
+        public void handle(WindowEvent event) {
+            final Alert sure = new Alert(CONFIRMATION,
+                    "Are you sure you want to exit?");
+            sure.setHeaderText("Are you sure?");
+            Optional<ButtonType> choice = sure.showAndWait();
+            if (choice.isPresent() &&
+                    choice.get().getText().equalsIgnoreCase("CANCEL")) {
+                event.consume();
+            }
+        }
     }
 }
